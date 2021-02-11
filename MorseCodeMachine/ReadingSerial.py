238c384883b4f743de5493092926c40298666d89
Sergi <78795405+SeergiCureero@users.noreply.github.com>
2021-02-11 21:10:16 +0100

    Update ReadingSerial.py

--- MorseCodeMachine/ReadingSerial.py
+++ MorseCodeMachine/ReadingSerial.py
@@ -1,5 +1,5 @@
 import serial, time
-arduino = serial.Serial('COM3', 9600)
+arduino = serial.Serial('COM3', 9600)   #'COM3' is the port where I plugged my arduino. Make sure you change it to the port where you are plugging your arduino
 time.sleep(2)
 rawString = arduino.readline()
 print(rawString)
