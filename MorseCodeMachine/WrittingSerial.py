a46917d364019ab02a134bdbbf3e47b5b5d5f97c
Sergi <78795405+SeergiCureero@users.noreply.github.com>
2021-02-11 21:04:47 +0100

    Update WrittingSerial.py

--- MorseCodeMachine/WrittingSerial.py
+++ MorseCodeMachine/WrittingSerial.py
@@ -1,5 +1,5 @@
 import serial, time
-arduino = serial.Serial("COM3", 9600)
+arduino = serial.Serial("COM3", 9600)   #The port where you plug your Arduino
 time.sleep(2)
 arduino.write(b'9')
 arduino.close()
